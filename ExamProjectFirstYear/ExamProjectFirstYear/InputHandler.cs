e85202f567d7d3ced98014a9b77631088332dbbd
GitGutAtGitHub <58419128+GitGutAtGitHub@users.noreply.github.com>
2020-05-21 16:03:54 +0200

    Setup continued

---
+++ ExamProjectFirstYear/ExamProjectFirstYear/InputHandler.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProjectFirstYear
+{
+	public class InputHandler
+	{
+		#region Fields
+		private Dictionary<Keys, ICommand> keyBinds = new Dictionary<Keys, ICommand>();
+
+		private static InputHandler instance;
+		#endregion
+
+		#region PROPERTIES
+		public static InputHandler Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new InputHandler();
+				}
+				return instance;
+			}
+		}
+
+		#endregion
+
+		#region METHODS
+		 /// <summary>
+        /// Adds all controls for player to a keybinds Dictionary once instantiated.
+        /// </summary>
+        private InputHandler()
+        {
+            // Moves player left when pressing A.
+            keyBinds.Add(Keys.A, new MoveCommand(new Vector2(-1, 0)));
+            // Moves player right when pressing D.
+            keyBinds.Add(Keys.D, new MoveCommand(new Vector2(1, 0)));
+        }
+
+		/// <summary>
+		/// Executes all commands for the keys added to the dictionary
+		/// </summary>
+		/// <param name="player"></param>
+		public void Execute(Player player)
+		{
+			KeyboardState keyState = Keyboard.GetState();
+
+			foreach (Keys key in keyBinds.Keys)
+			{
+				if (keyState.IsKeyDown(key))
+				{
+					keyBinds[key].Execute(player);
+				}
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ ExamProjectFirstYear/ExamProjectFirstYear/MoveCommand.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProjectFirstYear
+{
+	class MoveCommand : ICommand
+	{
+		public MoveCommand(Vector2 velocity)
+		{
+
+		}
+
+		/// <summary>
+		/// Executes the command
+		/// </summary>
+		/// <param name="player"></param>
+		public void Execute(Player player)
+		{
+
+		}
+	}
+}
