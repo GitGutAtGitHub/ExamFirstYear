a509a2807b112dffeb6b90b2aeedaa7742118f24
KuMoJo <KuMoJo@users.noreply.github.com>
2020-05-21 15:51:29 +0200

    SQLiteHandler implementeret
    Tilføjer database og logik til programmet.

---
+++ ExamProjectFirstYear/Testing/Database.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExamProjectFirstYear;
+using System.Data.SQLite;
+
+namespace Testing
+{
+    /// <summary>
+    /// For testing SQLiteHandler methods.
+    /// </summary>
+    [TestClass]
+    public class Database
+    {
+        SQLiteConnection connection;
+        SQLiteCommand command;
+
+
+        [TestMethod]
+        public void LoadConnectionStringTest()
+        {
+            ////Arrange
+            //SQLiteConnection connection;
+            //string connectionStringActual;
+            //var connectionStringExpected = SQLiteConnection;
+
+            ////Act
+            //connection = new SQLiteConnection(SQLiteHandler.LoadConnectionString());
+
+            //connectionStringActual = connection.ToString();
+
+            ////Assert
+            //Assert.AreEqual(connectionStringActual, connectionStringExpected);
+        }
+
+        public void ExecuteNonQuerySQLiteCommandTest()
+        {
+            //Arrange
+            //connection = new SQLiteConnection(SQLiteHandler.LoadSQLiteConnectionString());
+
+            ////Act
+            //using (connection)
+            //{
+            //    connection.Open();
+            //    command = new SQLiteCommand(commandText, connection);
+            //    command.ExecuteNonQuery();
+            //}
+
+            ////Assert
+        }
+    }
+}
---
+++ ExamProjectFirstYear/Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b89e8aa-00f1-4b1a-8cb3-b7add1cd8073")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
