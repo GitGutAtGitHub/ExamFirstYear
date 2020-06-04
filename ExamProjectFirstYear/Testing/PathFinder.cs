0da00a57603928db0b0668cd3e83ec10ba197287
thecatmiaw <58418706+thecatmiaw@users.noreply.github.com>
2020-06-04 13:04:52 +0200

    Unit pis

---
+++ ExamProjectFirstYear/Testing/PathFinder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamProjectFirstYear.PathFinding;
+using ExamProjectFirstYear;
+
+namespace Testing
+{
+    [TestClass]
+    class PathFinder
+    {
+        [TestMethod]
+        public void FindMeleeTargetNode()
+        {
+
+        }
+
+        [TestMethod]
+        public void CellRowCount()
+        {
+
+
+
+        }
+       
+    }
+}
